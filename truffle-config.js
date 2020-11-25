1a9a027e72d33bdcc78bfd3cca6e09042b88aabe
lisun85 <70138362+lisun85@users.noreply.github.com>
2020-11-25 17:22:15 +0800

    Commit

--- truffle-config.js
+++ truffle-config.js
@@ -24,6 +24,15 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "fj4jll3k.....";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraURL = 'https://rinkeby.infura.io/v3/9b03003aa3c645088005eca4da1f5afc'
+
+var HDWallet = require('@truffle/hdwallet-provider')
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -36,43 +45,37 @@ module.exports = {
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
+    development: {
+        host: 'localhost',
+        port: 7545,
+        network_id: '*'
+    },
+    
+    main: {
+      provider: () => new HDWalletProvider(mnemonic,`https://mainnet.infura.io/v3/9b03003aa3c645088005eca4da1f5afc`),
+      network_id: 1,
+      gas: 6500000,
+    },
+    
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/9b03003aa3c645088005eca4da1f5afc"),
+      network_id: 3,
+      gas: 5500000,
+    },
+    
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/9b03003aa3c645088005eca4da1f5afc"),
+      network_id: 42,
+      gas: 5500000,
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4,          // Rinkeby's network id
+      gas: 5500000,        
+    },
+
+},
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
